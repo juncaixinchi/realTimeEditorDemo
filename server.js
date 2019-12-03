693a88bf6ff96584db4ad4c143347bc1f6d4f77d
lixinwei <xwli@openailab.com>
2019-12-03 09:28:14 +0800

    init commit

---
+++ server.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use('/', express.static(__dirname + '/build'));
+
+server.listen(8000);
+
+let file = fs.readFileSync(__dirname + '/test/index.js').toString();
+
+io.on('connection', function (socket) {
+  socket.emit('doc', {
+    timestamp: new Date().getTime(),
+    doc: file
+  })
+  socket.on('update', function (data) {
+    file = data.doc;
+    io.emit('doc', data);
+  })
+
+  socket.on('disconnect', function (data) {
+    console.log('disconnect', data)
+  })
+});
--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
+import io from 'socket.io-client';
 import logo from './logo.svg';
+import CodeMirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { msg: '' };
+    this.currentDoc = { timestamp: -1, doc: '' }
+  }
+  componentDidMount() {
+
+    const socket = io('http://localhost:8000');
+
+    this.codemirror = CodeMirror(this.ref, {
+      value: this.currentDoc.doc,
+      mode: "javascript",
+      lineNumbers: true,
+    });
+
+    socket.on('connect', function () {
+      console.log('connect')
+    });
+
+    socket.on('disconnect', function () {
+      console.log('disconnect')
+    });
+
+    socket.on('doc', (data) => {
+      const { timestamp, doc } = data
+      if (timestamp > this.currentDoc.timestamp) {
+        this.currentDoc = { timestamp, doc };
+        if (doc !== this.codemirror.getValue()) {
+          this.codemirror.setValue(doc)
+        }
+      }
+    });
+
+    this.codemirror.on('change', (...args) => {
+      socket.emit('update', {
+        timestamp: new Date().getTime(),
+        doc: this.codemirror.getValue()
+      })
+    })
+  }
+  render() {
+    return (
+      <div>
+        <div style={{ color: 'black', margin: 0, padding: 0 }} ref={(ref) => this.ref = ref} />
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
